b95826a8f130dfcd3d3380053d532807cc9e0e9c
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2021-09-27 15:17:44 -0300

    Python

---
+++ Python/inicio.py
@@ -0,0 +1,15 @@
+# permite imprimir por la consola
+print("hola mundo")
+# permite leer el teclado o recibir datos
+input("ingrese su nombre:")
+
+# crear una variable
+variable1 = 10 # int / numero
+variable2 = "hola" # string / texto
+
+print(variable2)
+
+nombre = ""
+nombre = input("¿Cual es tú nombre?: ")
+print(nombre)
+print("Hola", nombre)
